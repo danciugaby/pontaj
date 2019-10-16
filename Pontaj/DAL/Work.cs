447b8cac40aca7f2e567196d7d78fc0c0e441032
alextarba <alextarba14@gmail.com>
2019-10-16 16:43:09 +0300

    Added work class

---
+++ Pontaj/DAL/Work.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Work
+    {
+        private User user;
+        private ClockingType type;
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public Work(User user, ClockingType type, DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            User = user;
+            Type = type;
+        }
+        public DateTime StartDate { get => startDate; set => startDate = value; }
+        public DateTime EndDate { get => endDate; set => endDate = value; }
+        public User User { get => user; set => user = value; }
+        public ClockingType Type { get => type; set => type = value; }
+
+        public override bool Equals(object obj)
+        {
+            var work = obj as Work;
+            return work != null &&
+                   StartDate == work.StartDate &&
+                   EndDate == work.EndDate &&
+                   EqualityComparer<User>.Default.Equals(User, work.User) &&
+                   EqualityComparer<ClockingType>.Default.Equals(Type, work.Type);
+        }
+    }    
+}
